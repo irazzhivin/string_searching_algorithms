4244d19d7c9c5bccaa21762232af4061f0fabb6b
Ivan Razzhivin <irazzhivin@icloud.com>
2021-06-19 17:39:10 +0300

    form_pi optimized

--- K_Morris/l_morris.py
+++ K_Morris/l_morris.py
@@ -1,17 +1,12 @@
 def form_pi(ps):
-    pi = [0 for _ in range(len(ps))]
-    i, j = 0, 1
-    while j < len(ps):
-        if ps[i] == ps[j]:
-            pi[j] = i + 1
-            i += 1
-            j += 1
-        else:
-            if i == 0:
-                pi[j] = 0
-                j += 1
-            else:
-                i = pi[i - 1]
+    output = [0]
+    for i in range(1, len(st)):
+        j = output[i - 1]
+        while j > 0 and st[j] != st[i]:
+            j = output[j - 1]
+        if st[j] == st[i]:
+            j = j + 1
+        output.append(j)
     return pi
 
 
