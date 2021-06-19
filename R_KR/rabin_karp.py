3b8999d5b2107a5da967c96c81f6529ea1fec6f8
Ivan Razzhivin <irazzhivin@icloud.com>
2021-06-19 16:09:14 +0300

    Rabin opt

--- R_KR/rabin_karp.py
+++ R_KR/rabin_karp.py
@@ -1,39 +1,32 @@
-def calc_hash(h, wp, wn, lps, p, x):
-    h = ((h - ord(wp)*(x**(lps - 1)))*x + ord(wn)) % p
-    return h
+def rabin_karp(st, subst, q=101):
+    compares = 0
+    M = len(subst)
+    N = len(st)
+    indices = []
+    d = 512
 
+    j = 0
+    p = 0  # hash subst
+    t = 0  # hash st
+    h = 1
 
-def str_check(s1, s2):
-    n = 0
-    for i in range(len(s1)):
-        n += 1
-        if s1[i] != s2[i]:
-            return (0, n)
-    return (1, n)
+    for i in range(M - 1):
+        h = (h * d) % q
 
+    for i in range(M):
+        p = (d * p + ord(subst[i])) % q
+        t = (d * t + ord(st[i])) % q
 
-def rabin(s, ps, p):
-    n = 0
-    x = random.randint(0, p - 1)
-    ls = len(s)
-    lps = len(ps)
-    h = sum(ord(s[i - 1])*(x**(lps - i)) for i in range(1, lps)) % p
-    hps = sum(ord(ps[i - 1])*(x**(lps - i)) for i in range(1, lps)) % p
-    n += 1
-    if h == hps:
-        ch = str_check(s[0:lps], ps)
-        n += ch[1]
-        if ch[0]:
-            print(n, ' сравнений')
-            return 0
-    for i in range(1, ls - lps + 1):
-        h = sum(ord(s[i + j - 1])*(x**(lps - j)) for j in range(1, lps)) % p
-        n += 1
-        if h == hps:
-            ch = str_check(s[i:i + lps], ps)
-            n += ch[1]
-            if ch[0]:
-                print(n, ' сравнений')
-                return i
-    print(n, ' сравнений')
-    return None
+    for i in range(N - M + 1):
+        compares += 1
+        if p == t:
+            for j in range(M):
+                compares += 1
+                if st[i + j] != subst[j]:
+                    break
+            j += 1
+            compares += 1
+            if j == M:
+                indices.append(i)
+
+    return indices, compares
