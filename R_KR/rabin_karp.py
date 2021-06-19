e03bdce4a701db360b828672ac8050ec8400ad6f
Ivan Razzhivin <irazzhivin@icloud.com>
2021-06-19 16:13:52 +0300

    Update rabin_karp.py

--- R_KR/rabin_karp.py
+++ R_KR/rabin_karp.py
@@ -1,22 +1,18 @@
+d = 256#Alphabet
 def rabin_karp(st, subst, q=101):
     compares = 0
     M = len(subst)
     N = len(st)
     indices = []
-    d = 256
-
     j = 0
     p = 0  # hash subst
     t = 0  # hash st
     h = 1
-
     for i in range(M - 1):
         h = (h * d) % q
-
     for i in range(M):
         p = (d * p + ord(subst[i])) % q
         t = (d * t + ord(st[i])) % q
-
     for i in range(N - M + 1):
         compares += 1
         if p == t:
@@ -28,7 +24,12 @@ def rabin_karp(st, subst, q=101):
             compares += 1
             if j == M:
                 indices.append(i)
-
+        compares += 1
+        if i < N - M:
+            t = (d * (t - ord(st[i]) * h) + ord(st[i + M])) % q
+            compares += 1
+            if t < 0:
+                t = t + q
     return indices, compares
 
 #CHeck 
