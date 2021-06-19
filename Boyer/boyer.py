e0e2ac1b84e7bd75200a0fc3f25a31eb27b4f3e2
Ivan Razzhivin <irazzhivin@icloud.com>
2021-06-19 17:22:06 +0300

    Boyer–Moore algorithm

---
+++ Boyer/boyer.py
@@ -0,0 +1,32 @@
+def boyer_moore_horspool(st, subst):
+    indices = []  # index massive
+    compares = 0
+    unique = set()  # substr unic symbols 
+    offsets = {}  # offsets alphabet
+    # offsets alphabet initilization
+    # move all symbols, but not move last
+    for i in range(len(subst) - 2, -1, -1):  # before lasr
+        if subst[i] not in unique:  # condition if symb no add into alphabet
+            unique.add(subst[i])
+            offsets[subst[i]] = len(subst) - i - 1  # m=6
+    # search substr in str
+    k = len(subst) - 1
+    while k < len(st):
+        compares += 1
+        j = len(subst) - 1  # count for checked symb in substr
+        i = k  # count for checked symb in main str
+        while j >= 0 and st[i] == subst[j]:
+            compares += 2
+            j -= 1
+            i -= 1
+        compares += 1
+        if j == len(subst) - 1:  # last symb not same
+            if offsets.get(st[i]) is None:
+                off = offsets['*']
+            else:
+                off = offsets[st[i]]
+        compares += 1
+        if j == -1:  # FIND successfully!!!
+            indices.append(i + 1)
+        k += off
+    return indices, compares
