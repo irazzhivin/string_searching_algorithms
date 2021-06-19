1a2b220c7710aa65e4f9d71fa1fd7a9832242924
Ivan Razzhivin <irazzhivin@icloud.com>
2021-06-19 17:37:44 +0300

    Update l_morris.py

--- K_Morris/l_morris.py
+++ K_Morris/l_morris.py
@@ -13,3 +13,26 @@ def form_pi(ps):
             else:
                 i = pi[i - 1]
     return pi
+
+
+def kmp(s, ps):
+    pi = form_pi(ps)
+    ls = len(s)
+    lps = len(ps)
+    i, j = 0, 0
+    n = 0
+    while i < ls:
+        n += 1
+        if s[i] == ps[j]:
+            i += 1
+            j += 1
+            if j == lps:
+                print(n, ' сравнений')
+                return i - lps
+        else:
+            if j == 0:
+                i += 1
+            else:
+                j = pi[j - 1]
+    print(n, ' сравнений')
+    return None
